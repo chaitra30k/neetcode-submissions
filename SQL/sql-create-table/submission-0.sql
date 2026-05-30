9359da08b2115dd72ddc0a123f6e6a09f4f217a5
Chaitra Kankanameli <132379675+chaitra30k@users.noreply.github.com>
2026-05-31 00:47:04 +0530

    Add: sql-create-table - submission-0

---
+++ SQL/sql-create-table/submission-0.sql
@@ -0,0 +1,12 @@
+Create table videos(id int, name text, created_at date, published boolean);
+
+
+
+
+
+
+
+-- Do not modify below this line --
+SELECT table_name, column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'videos';
